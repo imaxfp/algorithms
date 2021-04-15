1ec47c47dfe5837f10857dc14332a594d86cecf2
imaxfp@gmail.com <imaxfp@gmail.com>
2021-04-15 17:17:33 +0300

    [warming_up] added tests with solutions

--- src/test/python/warming_up.py
+++ src/test/python/warming_up.py
@@ -1,6 +1,7 @@
 import unittest
 
-from src.warming_up import find_hypotenuse
+from src.warming_up import find_hypotenuse, find_the_number_of_hundreds, clock, time_slot_difference, \
+    find_the_max, is_leap_year
 
 
 class MyTestCase(unittest.TestCase):
@@ -8,8 +9,31 @@ class MyTestCase(unittest.TestCase):
     def test_find_hypotenuse(self):
         self.assertEqual(find_hypotenuse(3, 4), 5)
 
-    # def test_collect_all_unique_words(self):
-    #    self.assertEqual(sum_of_numbers(179), 17)
+    def test_find_the_number_of_hundreds(self):
+        self.assertEqual(find_the_number_of_hundreds(1891), 8)
+        self.assertEqual(find_the_number_of_hundreds(11975), 9)
+
+    def test_clock(self):
+        self.assertEqual(clock(0), (0, 0))
+        self.assertEqual(clock(1), (0, 1))
+        self.assertEqual(clock(1449), (0, 9))
+        self.assertEqual(clock(2880), (24, 0))
+        self.assertEqual(clock(28811), (0, 11))
+
+    def test_time_slot_difference(self):
+        self.assertEqual(time_slot_difference(start_h=1, start_min=1, start_sec=0,
+                                              end_h=1, end_min=2, end_sec=2), 62)
+        self.assertEqual(time_slot_difference(start_h=1, start_min=1, start_sec=1,
+                                              end_h=2, end_min=2, end_sec=2), 3661)
+
+    def test_find_the_max(self):
+        self.assertEqual(find_the_max([1, 3, 5, 2, 1]), 5)
+
+    def test_leap_year(self):
+        self.assertEqual(is_leap_year(2007), False)
+        self.assertEqual(is_leap_year(2000), True)
+        self.assertEqual(is_leap_year(2021), False)
+        self.assertEqual(is_leap_year(2024), True)
 
 
 if __name__ == '__main__':
--- src/warming_up.py
+++ src/warming_up.py
@@ -1,4 +1,5 @@
 import math
+from functools import reduce
 
 
 def find_hypotenuse(a, b):
@@ -11,7 +12,7 @@ def find_hypotenuse(a, b):
     :param number:
     :return:
     """
-    return None
+    return math.sqrt(a ** 2 + b ** 2)
 
 
 def find_the_number_of_hundreds(number):
@@ -19,7 +20,8 @@ def find_the_number_of_hundreds(number):
     For example:
     1975 -> 9
     """
-    return None
+
+    return [int(i) for i in str(number)][-3]
 
 
 def sum_of_numbers(number):
@@ -27,28 +29,37 @@ def sum_of_numbers(number):
     For example:
     179 -> 17
     """
-    return None
+    # return sum([int(i) for i in str(number)])
+    # return reduce(lambda a, b: a + b, [int(i) for i in str(number)])
+    tmp = 0
+    for i in [int(i) for i in str(number)]:
+        tmp += i
+    return tmp
 
 
 def clock(minutes):
     """
     Program should return amount of hours and minutes as a tuple.
-    Pay attention if more than 24h in minutes clock shout reset amount of hours to 0
+    Pay attention if more than 24h in minutes clock should reset amount of hours to 0
     For example:
     150 -> 2:30
     1441 -> 0:1
     """
-    return None
+    if minutes == 0: return 0, 0
+    while minutes > 1440: minutes -= 1440
+    return int(minutes / 60), minutes % 60
 
 
-def time_slot_difference(minutes):
+def time_slot_difference(start_h, start_min, start_sec, end_h, end_min, end_sec):
     """
     You have two time slots with 3 numbers - hours, minutes, seconds in one 24 hour period of time.
     find the difference in seconds in this period
     For example:
     1,1,1 and 2,2,2 -> 3661
     """
-    return None
+    start_in_sec = (start_h * 60 * 60) + (start_min * 60) + start_sec
+    end_in_sec = (end_h * 60 * 60) + (end_min * 60) + end_sec
+    return end_in_sec - start_in_sec
 
 
 def find_the_max(arr_numbers):
@@ -56,14 +67,18 @@ def find_the_max(arr_numbers):
     For example:
     1,1,4,7 -> 7
     """
-    return None
+    # return max(arr_numbers)
+    max = 0
+    for i in arr_numbers:
+        if max < i: max = i
+    return max
 
 
-def leap_year(year):
+def is_leap_year(year):
     """
     Return true if year is leap (the year is leap if year multiple 4 and NOT multiple by 100. Or  multiple by 400)
     For example:
     2007 -> FALSE
     2000 -> TRUE
     """
-    return None
+    return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
