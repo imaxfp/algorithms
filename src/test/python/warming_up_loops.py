43d6dd6e03eb1ac517c93b6ba2ea1f42c34ca14d
imaxfp@gmail.com <imaxfp@gmail.com>
2021-04-20 12:19:24 +0300

    [warming_up_loops] basic classes

--- src/test/python/warming_up_loops.py
+++ src/test/python/warming_up_loops.py
@@ -1,17 +1,33 @@
 import unittest
-
-from src.warming_up_numbers import find_hypotenuse, find_the_number_of_hundreds, clock, time_slot_difference, \
-    find_the_max, is_leap_year
+from src.warming_up_loops import even_numbers_range, factorial, divisors_of_a_number, list_of_squares, minimal_divisor, \
+    find_count_of_numbers_greater_then_index_number
 
 
 class MyTestCase(unittest.TestCase):
 
-    def test_find_hypotenuse(self):
-        self.assertEqual(find_hypotenuse(3, 4), 5)
+    def test_even_numbers_range(self):
+        self.assertEqual(even_numbers_range(1, 43),
+                         [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42])
 
-    def test_find_the_number_of_hundreds(self):
-        self.assertEqual(find_the_number_of_hundreds(1891), 8)
-        self.assertEqual(find_the_number_of_hundreds(11975), 9)
+    def test_factorial(self):
+        self.assertEqual(factorial(3), 6)
+        self.assertEqual(factorial(4), 24)
+        self.assertEqual(factorial(8), 40320)
+        self.assertEqual(factorial(14), 87178291200)
+
+    def test_divisors_of_a_number(self):
+        self.assertEqual(divisors_of_a_number(32), [1, 2, 4, 8, 16, 32])
+
+    def test_list_of_squares(self):
+        self.assertEqual(list_of_squares(15), [1, 4, 9])
+
+    def test_minimal_divisor(self):
+        self.assertEqual(minimal_divisor(15), 3)
+
+    def test_find_count_of_numbers_greater_then_index_number(self):
+        self.assertEqual(find_count_of_numbers_greater_then_index_number([1, 2, 3, 4, 5], 0), 4)
+        self.assertEqual(find_count_of_numbers_greater_then_index_number([10, 2, 3, 4, 5], 0), 0)
+        self.assertEqual(find_count_of_numbers_greater_then_index_number([3, 2, 3, 10, 1], 1), 2)
 
 
 if __name__ == '__main__':
--- src/warming_up_loops.py
+++ src/warming_up_loops.py
@@ -1,23 +1,79 @@
 import math
+from functools import reduce
+
+from numpy import long
 
 
 def even_numbers_range(a, b):
     """
     Return all even numbers from 'a' to 'b'
     For example:
-    a = 2
-    b = 5
-    return = [2,4]
-    :param number:
-    :return:
+    :param a: = 2
+    :param b: = 5
+    :return: [2,4]
+
     """
     return list(filter(lambda x: x % 2 == 0, range(a, b)))
 
 
-def find_the_number_of_hundreds(number):
+def factorial(n):
+    """
+    Get factorial of the number 'n'
+    :param n: 4
+    :return: 24
+    """
+    return long(reduce(lambda x, y: x * y, [i for i in range(1, n + 1)]))
+
+
+def divisors_of_a_number(n):
+    """
+    :param n: 32
+    :return: 1 2 4 8 16 32
+    """
+    return list(filter(lambda i: n % i == 0, [i for i in range(1, n + 1)]))
+
+
+def list_of_squares(n):
+    """
+    Return list of all squares but not more than 'n'
+    :param n: 15
+    :return: 1,4,9
+    """
+    l = list()
+    for i in range(1, n):
+        if i ** 2 < n:
+            l.append(i ** 2)
+        else:
+            break
+    return l
+
+
+def minimal_divisor(n):
+    """
+    Find the minimal divisor of the 'n'
+    :param n: 15
+    :return: 3
+    """
+    min_div = 0
+    for i in range(2, n):
+        if n % i == 0:
+            min_div = i
+            break
+    return min_div
+
+
+def find_count_of_numbers_greater_then_index_number(arr, index):
     """
     For example:
-    1975 -> 9
+    :param arr: [1 2 3 4 5]
+    :param index: 0
+    :return: 4
     """
+    c = 0
+    for i in range(index + 1, len(arr)):
+        if arr[i] > arr[index]:
+            c += 1
+        else:
+            break
 
-    return [int(i) for i in str(number)][-3]
+    return c
