91e2950168a86d136a5b9297ee4871b3eb127b93
imaxfp@gmail.com <imaxfp@gmail.com>
2021-04-24 20:38:13 +0300

    [warming_up_loops] prepared code for the warming up

--- src/recursions.py
+++ src/recursions.py
@@ -1,7 +1,40 @@
-def reverse_array_rec(arr):
+def reverse_array_classic(arr):
     """
     :param arr: 1,2,3,4,5
     :return: 5,4,3,2,1
+    """
+    # return list.reverse(arr)
+
+    # 1 find indexes for swap elements
+    head = 0
+    tail = len(arr) - 1
+
+    # 2 stop reverse if middle array established
+    # 2 TODO or
+
+    # algo for swapping elements places
+    while head != tail:
+        print(arr)
+        tmp = arr[head]
+        arr[head] = arr[tail]
+        arr[tail] = tmp
+        head += 1
+        tail -= 1
+
+    return arr
+
+
+def reverse_array_rec(arr, head, tail):
+    """
+    # 1. detect the condition for STOP recursion
+    # 2. send to recursion 'arr' and parameters 'head' and 'tail'
+    # 3. implement the logic of recursion
+    # 4. call self
+
+    :param arr:
+    :param head:
+    :param tail:
+    :return:
     """
     return None
 
@@ -18,5 +51,4 @@ def fibonacci_number(n):
     Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
     0, 1, 1, 2, 3, 5, 8, 13
     """
-
     return None
--- src/test/python/recursions.py
+++ src/test/python/recursions.py
@@ -1,9 +1,16 @@
 import unittest
 
-from src.recursions import fibonacci_number, reverse_array_rec
+from src.recursions import fibonacci_number, reverse_array_rec, reverse_array_classic
 
 
 class MyTestCase(unittest.TestCase):
+
+    def test_reverse_array(self):
+        self.assertEqual(reverse_array_classic([1, 2, 3, 4, 5]), [5, 4, 3, 2, 1])
+
+    def test_reverse_rec(self):
+        arr = [1, 2, 3, 4, 5, 6]
+        self.assertEqual(reverse_array_rec(arr, 0, len(arr) - 1), [6, 5, 4, 3, 2, 1])
 
     def test_fibonacci_number(self):
         self.assertEqual(fibonacci_number(1), 1)
@@ -11,9 +18,6 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(fibonacci_number(3), 2)
         self.assertEqual(fibonacci_number(4), 3)
         self.assertEqual(fibonacci_number(100), 354224848179261915075)
-
-    def test_reverse_array(self):
-        self.assertEqual(reverse_array_rec([1, 2, 3, 4, 5]), [5, 4, 3, 2, 1])
 
 
 if __name__ == '__main__':
