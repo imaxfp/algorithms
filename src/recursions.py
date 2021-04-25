bdc8ad496b8e317ca50dc6faf25ae74656616ea5
imaxfp@gmail.com <imaxfp@gmail.com>
2021-04-25 14:29:10 +0300

    [warming_up_loops] prepared code for the warming up

--- src/recursions.py
+++ src/recursions.py
@@ -1,17 +1,14 @@
-def reverse_array_rec(arr):
+from Cython.Compiler.ExprNodes import ListNode
+
+
+def reverse_array_rec(arr, head, tail):
     """
     :param arr: 1,2,3,4,5
     :return: 5,4,3,2,1
     """
-
-    def swap(arr, first, last):
-        if first == last: return arr
-        tmp = arr[first]
-        arr[first] = arr[last]
-        arr[last] = tmp
-        return swap(arr, first + 1, last - 1)
-
-    return swap(arr, 0, len(arr) - 1)
+    if head >= tail: return arr
+    arr[head], arr[tail] = arr[tail], arr[head]
+    return reverse_array_rec(arr, head + 1, tail - 1)
 
 
 def fibonacci_number(n):
@@ -32,3 +29,58 @@ def fibonacci_number(n):
         return fibonacci_number(current, previous + current, iteration + 1, n)
 
     return fibonacci_number(0, 1, 1, n)
+
+
+def add_two_numbers(l1, l2):
+    """
+    You are given two non-empty linked lists representing two non-negative integers.
+    The digits are stored in reverse order, and each of their nodes contains a single digit.
+    Add the two numbers and return the sum as a linked list.
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+    Input: l1 = [2,4,3], l2 = [5,6,4]
+    Output: [7,0,8]
+    Explanation: 342 + 465 = 807.
+    """
+
+    def add_two_numbers(l1, l2, index, dozens, res_l: list) -> list:
+        if index >= len(l1): return res_l
+        sum = l1[index] + l2[index]
+        if dozens == 1:
+            sum += 1
+            dozens = 0
+        if sum >= 10:
+            sum -= 10
+            dozens = 1
+        res_l.append(sum)
+        return add_two_numbers(l1, l2, index + 1, dozens, res_l)
+
+    return add_two_numbers(l1, l2, 0, 0, list())
+
+
+def letter_combinations_phone_number(num):
+    """
+    Given a string containing digits from 2-9 inclusive, 
+    return all possible letter combinations that the number could represent. Return the answer in any order.
+    A mapping of digit to letters (just like on the telephone buttons) is given below. 
+    Note that 1 does not map to any letters.
+
+    Input: digits = "23"
+    Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]    
+    :return: 
+    """
+
+    def brute_force_rec(latter_index, first_arr, second_arr, res: list) -> list:
+        if len(first_arr) <= latter_index: return res
+        first_latter = first_arr[latter_index]
+        res.extend(list(map(lambda x: first_latter + x, second_arr)))
+        return brute_force_rec(latter_index + 1, first_arr, second_arr, res)
+
+    tel = {2: "abc", 3: "def", 4: "ghi", 5: "jkl", 6: "mno", 7: "pqrs", 8: "tuv", 9: "wxyz"}
+    arr_c = list(map(lambda x: tel.get(x), [int(x) for x in str(num)]))
+
+    return brute_force_rec(0, [s for s in str(arr_c[0])], [s for s in str(arr_c[1])], list())
+
+
+if __name__ == "__main__":
+    print()
--- src/test/python/recursions.py
+++ src/test/python/recursions.py
@@ -1,6 +1,5 @@
 import unittest
-
-from src.recursions import fibonacci_number, reverse_array_rec
+from src.recursions import fibonacci_number, reverse_array_rec, add_two_numbers, letter_combinations_phone_number
 
 
 class MyTestCase(unittest.TestCase):
@@ -13,7 +12,16 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(fibonacci_number(100), 354224848179261915075)
 
     def test_reverse_array(self):
-        self.assertEqual(reverse_array_rec([1, 2, 3, 4, 5]), [5, 4, 3, 2, 1])
+        arr = [1, 2, 3, 4, 5, 6]
+        self.assertEqual(reverse_array_rec(arr, 0, len(arr) - 1), [6, 5, 4, 3, 2, 1])
+
+    def test_add_two_numbers(self):
+        l1 = [2, 4, 3]
+        l2 = [5, 6, 4]
+        self.assertEqual(add_two_numbers(l1, l2), [7, 0, 8])
+
+    def test_letter_combinations_phone_number(self):
+        self.assertEqual(letter_combinations_phone_number(23), ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"])
 
 
 if __name__ == '__main__':
